3c1ac8c13c732f1093034df67d3fbea34d146289
Alex Frantz <alexandros.frantz@hotmail.com>
2021-03-13 14:05:18 +0100

    updated Book Shop class and added test cases

--- src/main/java/books/BookShop.java
+++ src/main/java/books/BookShop.java
@@ -1,24 +1,73 @@
 package books;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 public class BookShop {
 
-    private final String name;
+	private final String bookShopName;
+	private int bookPrice;
 
-    /**
-     * Constructor of the class Book shop
-     * @param name name of the book shop
-     */
-    public BookShop(String name){
-        this.name = name;
-    }
+	/**
+	 * Constructor of the class Book shop
+	 * @param bookShopName name of the book shop
+	 * @param bookPrice price of a book
+	 */
+	public BookShop(String bookShopName, int bookPrice){
+		this.bookShopName = bookShopName;
+		this.bookPrice = bookPrice;
+	}
+	
+	/**
+	 * method to compute the cost of a basket
+	 * <p> Example basket = {1,2,3,0,0} </p> 
+	 * @param books array corresponding to the number of each harry potter book the client desire to buy (books.length should return 5)
+	 * @return the cost in euro with the discount
+	 * 
+	 */
+	public double cost(int [] books){
+		
+		int bookLength = books.length; // length of array containing purchased books
+		
+		double discount = 0.0; // discount applied to purchase
+		double scale = Math.pow(10, 2); // used to round 2 decimals after the .
+		
+		int uniqueBooks = 0; // number of different books
+		
+		int totalBooks = IntStream.of(books).sum(); // total amount of books to be bought
+		
+		if(bookLength == 5) {
+			for(int i = 0; i < bookLength; i++) {
+				if(books[i] > 0) {
+					++uniqueBooks;
+				}
+			}
+			
+			switch(uniqueBooks) { 
+			case 2:
+				discount = (uniqueBooks * this.bookPrice * 0.07 ); 
+				System.out.println("Discount: " + discount);
+				break;
+			case 3:
+				discount = (uniqueBooks * this.bookPrice * 0.14 );
+				System.out.println("Discount: " + discount);
+				break;
+			case 4:
+				discount = (uniqueBooks * this.bookPrice * 0.28 );
+				System.out.println("Discount: " + discount);
+				break;
+			case 5:
+				discount = (uniqueBooks * this.bookPrice * 0.35 );
+				System.out.println("Discount: " + discount);
+				break;
 
-    /**
-     * method to compute the cost of a basket
-     * @param books array corresponding to the number of each harry potter book the client desire to buy (books.length should return 5)
-     * @return the cost in euro with the discount
-     */
-    public double cost(int[] books){
-        //TODO complete
-        return 0.0;
-    }
+			}
+
+		}else {
+			System.out.println("Book array is contains more or less entries than the supposed 5");
+			return - 1.0;
+		}
+		return this.bookPrice * totalBooks - discount;
+	}
 }
--- src/test/java/books/BookShopTest.java
+++ src/test/java/books/BookShopTest.java
@@ -3,6 +3,9 @@ package books;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
 /**
  * TODO : Add your import here
  */
@@ -11,10 +14,112 @@ public class BookShopTest {
 
     /**
      * TODO : Put your test here
+     * 
      */
+	private BookShop bookStore;
+	private final int bookStorePrice = 8;
+	private final double scaleFactor = Math.pow(10, 2);
 
+	@BeforeEach
+	public void setUp() throws Exception{ 
+		  this.bookStore = new BookShop("Harry Poter Book Store", this.bookStorePrice);
+	}
+	
     @Test
-    public void myFirstTest(){
-        assertTrue(false);
+    @DisplayName("1 Unique Book")
+    public void uniqueBook(){
+    	int [] uniqueBookSet1 = {1,0,0,0,0};
+    	int [] uniqueBookSet2 = {0,1,0,0,0};
+    	int [] uniqueBookSet3 = {0,0,1,0,0};
+    	int [] uniqueBookSet4 = {0,0,0,1,0};
+    	int [] uniqueBookSet5  = {0,0,0,0,1};
+        assertEquals(8.0,this.bookStore.cost(uniqueBookSet1));
+        assertEquals(8.0,this.bookStore.cost(uniqueBookSet2));
+        assertEquals(8.0,this.bookStore.cost(uniqueBookSet3));
+        assertEquals(8.0,this.bookStore.cost(uniqueBookSet4));
+        assertEquals(8.0,this.bookStore.cost(uniqueBookSet5));
     }
+    
+    
+    @Test
+    @DisplayName("2 Different Books")
+    public void differentBooks1() {
+    	double price = 2 * 8 * (1 - 0.07);
+    	int [] booksDifferent1 = {1,0,1,0,0};
+    	int [] booksDifferent2 = {0,1,0,1,0};
+    	int [] booksDifferent3 = {1,1,0,0,0};
+    	assertEquals(price,this.bookStore.cost(booksDifferent1));
+    	assertEquals(price,this.bookStore.cost(booksDifferent2));
+    	assertEquals(price,this.bookStore.cost(booksDifferent3));
+    }
+    
+    @Test
+    @DisplayName("3 Different Books")
+    public void differentBooks2() {
+    	double price = 3 * 8 * (1 - 0.14);
+    	int [] booksDifferent1 = {1,1,1,0,0};
+    	int [] booksDifferent2 = {0,1,1,1,0};
+    	int [] booksDifferent3 = {0,0,1,1,1};
+    	int [] booksDifferent4 = {1,0,1,0,1};
+    	int [] booksDifferent5 = {0,1,0,1,1};
+    	assertEquals(price,this.bookStore.cost(booksDifferent1));
+    	assertEquals(price,this.bookStore.cost(booksDifferent2));
+    	assertEquals(price,this.bookStore.cost(booksDifferent3));
+    	assertEquals(price,this.bookStore.cost(booksDifferent4));
+    	assertEquals(price,this.bookStore.cost(booksDifferent5));
+    } 
+    
+    
+    @Test
+    @DisplayName("4 Different books")
+    public void differentBooks3() { 
+    	double price = 4 * 8 * (1 - 0.28);
+    	int [] booksDifferent1 = {1,1,1,1,0};
+    	int [] booksDifferent2 = {0,1,1,1,1};
+    	int [] booksDifferent3 = {1,1,0,1,1};
+    	assertEquals(price,this.bookStore.cost(booksDifferent1));
+    	assertEquals(price,this.bookStore.cost(booksDifferent2));
+    	assertEquals(price,this.bookStore.cost(booksDifferent3));
+    }
+    
+    @Test
+    @DisplayName("5 Different books")
+    public void differentBooks4() { 
+    	double price = 5 * 8 * (1 - 0.35);
+    	int [] booksDifferent1 = {1,1,1,1,1};
+    	assertEquals(price,this.bookStore.cost(booksDifferent1));
+    }
+    
+    @Test
+    @DisplayName("3 different books and 2 duplicates")
+    public void differentAndDuplicate1() { 
+    	double price = 3 * 8 * (1 - 0.14) + 2 * 8;
+    	int [] bookSet1 = {1,1,3,0,0};
+    	assertEquals(price,this.bookStore.cost(bookSet1));
+    }
+    
+    @Test
+    @DisplayName("5 different books and 4 duplicates")
+    public void differentAndDuplicate2() { 
+    	double price = 5 * 8 * (1 - 0.35) + 4 * 8;
+    	int [] bookSet1 = {2,2,2,2,1};
+    	assertEquals(price,this.bookStore.cost(bookSet1));
+    }
+    
+    @Test
+    @DisplayName("4 different books and 5 duplicates")
+    public void differentAndDuplicate3() { 
+    	double price = 4 * 8 * (1 - 0.28) + 5 * 8;
+    	int [] bookSet1 = {1,5,2,1,0};
+    	assertEquals(price,this.bookStore.cost(bookSet1));
+    }
+    
+    @Test
+    @DisplayName("Empty book array")
+    public void emptyBookArray() { 
+    	double price = 0 - 1.0;
+    	int [] bookSet1 = {};
+    	assertEquals(price,this.bookStore.cost(bookSet1));
+    }
+    
 }
